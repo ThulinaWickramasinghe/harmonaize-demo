816ff29a1dde64922b9d8443ccffeecbe1d13bf6
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2024-05-20 03:06:03 +0530

    Initial commit

---
+++ HarmonAIze.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="HarmonAIze",
+    page_icon="🎵",
+)
+
+st.write("# Welcome to HarmonAIze! 👋")
+
+st.markdown(
+    """
+    HarmonAIze is your AI music mentor...\n
+    **👈 Select a model from the one's in the sidebar** to see what HarmonAIze can do.
+    ### Available features
+    - Evaluate a singing performance
+    - Identify voice type of a singer
+    - Idenfity a singer using his voice
+    - Analyse mood in a singing performance
+"""
+)
